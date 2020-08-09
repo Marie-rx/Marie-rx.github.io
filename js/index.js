3c64c90af9490683ee645a5862958ae9c268d7ae
Marie-rx <ronnaux.marie@gmail.com>
2020-08-09 18:37:56 +0200

    Fixed responsive bug when phone is on landscape

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ window.sr = ScrollReveal(/*{ reset: true }*/);
 $("[data-toggle='tooltip']").tooltip(); // Initialize Tooltip
 
 var minWidth200px = window.matchMedia("(min-width:200px)");
+var minWidth640px = window.matchMedia("(min-width:640px)");
 var minWidth1024px = window.matchMedia("(min-width:1024px)");
 
 $(document).ready(() => {  
@@ -331,6 +332,291 @@ if (minWidth200px.matches) {
 
        
     };
+    if (minWidth640px.matches) {
+    
+        $(".imgContainer").hover(function(){
+            $("#imgBg").show();
+            });
+    
+            //Anim banner
+            sr.reveal('#imgBg', {
+                duration: 500,
+                origin: 'bottom',
+                distance: '15rem',
+            });
+            sr.reveal('h1', {
+                duration: 1000,
+                origin: 'top',
+                distance: '1rem',
+            });
+            sr.reveal('p.lead', {
+                duration: 1000,
+                origin: 'left',
+                distance: '1rem',
+                viewFactor: 0.5,
+            });
+            sr.reveal('hr.text-left', {
+                duration: 1000,
+                origin: 'right',
+                distance: '1rem',
+            });
+    
+            //banner icons anim
+            sr.reveal('.gitIcon', {
+                duration: 1000,
+                origin: 'left',
+                distance: '2rem',
+                viewFactor: 0,
+            });
+            sr.reveal('.linkIcon', {
+                duration: 1000,
+                origin: 'left',
+                distance: '2rem',
+                delay: 100,
+                viewFactor: 0,
+            });
+            sr.reveal('.twitIcon', {
+                duration: 1000,
+                origin: 'left',
+                distance: '2rem',
+                delay: 200,
+                viewFactor: 0,
+            });
+            sr.reveal('.mailIcon', {
+                duration: 1000,
+                origin: 'left',
+                distance: '2rem',
+                delay: 300,
+                viewFactor: 0,
+            });
+            sr.reveal('.cvIcon', {
+                duration: 1000,
+                origin: 'left',
+                distance: '2rem',
+                delay: 400,
+                viewFactor: 0,
+            });
+    
+            //about anim skills
+            sr.reveal('#skills', {
+                duration: 1000,
+                origin: 'bottom',
+                distance: '2rem',
+                viewFactor: 1.0
+            });
+            sr.reveal('.Priority1', {
+                duration: 1000,
+                origin: 'left',
+                rotate: {
+                    y: 100,
+                },
+                scale: 0.75,
+                viewFactor: 0.5,
+                
+            });
+            sr.reveal('.Priority2', {
+                delay: 200,
+                duration: 1000,
+                origin: 'left',
+                rotate: {
+                    y: 100,
+                },
+                viewFactor: 0.5,
+                scale: 0.75
+            });
+            sr.reveal('.Priority3', {
+                duration: 1000,
+                origin: 'left',
+                rotate: {
+                    y: 100,
+                },
+                viewFactor: 0.5,
+                delay: 400,
+                scale: 0.75
+            });
+            sr.reveal('.Priority4', {
+                duration: 1000,
+                origin: 'left',
+                rotate: {
+                    y: 100,
+                },
+                viewFactor: 0.5,
+                delay: 600,
+                scale: 0.75
+            });
+            sr.reveal('.Profile', {
+                duration: 1000,
+                origin: 'bottom',
+                viewFactor: 0.5,
+                distance: '2rem',
+                viewFactor: 0.5,
+            });
+            sr.reveal('.allBars', {
+                duration: 500,
+                origin: 'bottom',
+                viewFactor: 0.5,
+                distance: '2rem',
+            });
+            sr.reveal('.bar1', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 200,
+    
+            });
+            sr.reveal('.bar2', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 300,
+    
+            });
+            sr.reveal('.bar3', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 400,
+    
+            });
+            sr.reveal('.bar4', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 500,
+    
+            });
+            sr.reveal('.bar5', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 600,
+    
+            });
+            sr.reveal('.bar6', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 700,
+                
+            });
+            sr.reveal('.bar7', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 800,
+    
+            });
+            sr.reveal('.bar8', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 900,
+    
+            });
+            sr.reveal('.bar9', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 1000,
+    
+            });
+            sr.reveal('.bar10', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 1100,
+    
+            });
+            sr.reveal('.bar11', {
+                duration: 1000,
+                origin: 'left',
+                distance: '3rem',
+                delay: 1200,
+    
+            });
+    
+            //Anim webdesign
+            sr.reveal('.animWeb', {
+                duration: 500,
+                viewFactor: 0.5,
+                rotate: {
+                    y: 100,
+                },
+                scale: 0.75
+            });
+    
+            //Anim Games
+            sr.reveal('.animGameLeft', {
+                duration: 700,
+                origin: 'bottom',
+                distance: '2rem',
+                viewFactor: 0.5,
+            });
+            sr.reveal('.animGameCenter', {
+                duration: 700,
+                origin: 'bottom',
+                distance: '2rem',
+                viewFactor: 0.5,
+                delay: 200,
+            });
+            sr.reveal('.animGameRight', {
+                duration: 700,
+                origin: 'bottom',
+                distance: '2rem',
+                viewFactor: 0.5,
+            });
+    
+            //Anim Graph
+            sr.reveal('.animGraphLeft', {
+                duration: 700,
+                origin: 'bottom',
+                distance: '2rem',
+                viewFactor: 0.5,
+            });
+            sr.reveal('.animGraphCenter', {
+                duration: 700,
+                origin: 'bottom',
+                distance: '2rem',
+                viewFactor: 0.5,
+                delay: 200,
+            });
+            sr.reveal('.animGraphRight', {
+                duration: 700,
+                origin: 'bottom',
+                distance: '2rem',
+                viewFactor: 0.5,
+            });
+    
+            //Anim Freelance
+            sr.reveal('.animFree1', {
+                duration: 700,
+                viewFactor: 0.5,
+                rotate: {
+                    y: 100,
+                },
+                scale: 1.2,
+            });
+            sr.reveal('.animFree2', {
+                duration: 700,
+                viewFactor: 0.5,
+                rotate: {
+                    y: 100,
+                },
+                delay: 200,
+                scale: 1.2,
+            });
+            sr.reveal('.animFree3', {
+                duration: 700,
+                viewFactor: 0.5,
+                rotate: {
+                    y: 100,
+                },
+                delay: 200,
+                scale: 1.2,
+            });
+    
+           
+        };
 //-------------------------------------END OF SMALL ANIMATIONS
 
 
